4950bd5380ceaf030b6ef9fc86d10c05580f4326
Moumouni BELEM <90107382+BELEMMoumouni@users.noreply.github.com>
2024-10-20 09:12:21 +0200

    Add project files.

---
+++ FasoQRCode/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FasoQRCode
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FasoQRCode/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FasoQRCode
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
