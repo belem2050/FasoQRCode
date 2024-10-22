f596e1656418d903ccd4e06ba4e328f06a19fb9a
Moumouni BELEM <90107382+BELEMMoumouni@users.noreply.github.com>
2024-10-22 23:30:35 +0200

    feat: add qr code reader zxing, cmaera swap and torch

--- FasoQRCode/MainPage.xaml.cs
+++ FasoQRCode/MainPage.xaml.cs
@@ -1,22 +1,82 @@
 ﻿using CommunityToolkit.Mvvm.Input;
-using FasoQRCode.Views;
 
 namespace FasoQRCode
 {
     public partial class MainPage : ContentPage
     {
+
+        public double ZoomRate
+        {
+            get
+            {
+                return _zoomRate;
+            }
+            set
+            {
+                _zoomRate = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        private double _zoomRate = 1;
         public MainPage()
         {
             InitializeComponent();
             BindingContext = this;
+
+            barcodeReader.Options = new ZXing.Net.Maui.BarcodeReaderOptions
+            {
+                Formats = ZXing.Net.Maui.BarcodeFormat.QrCode,
+                AutoRotate = true,
+                Multiple = true,
+                TryHarder = true,
+            };
+        }
+
+        private void barcodeReader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+        {
+            var first = e.Results?.FirstOrDefault();
+
+            if (first is null)
+            {
+                return;
+            }
+
+            Dispatcher.DispatchAsync(async () =>
+            {
+                await DisplayAlert("Barcode Detected", first.Value, "Burkina Faso is getting there");
+            });
         }
 
         [RelayCommand]
-        public async Task GoToTab()
+        public void ScanbyImage()
         {
-            //await Navigation.PushAsync(new PageMainTabbed());
-            //await Navigation.PushAsync(new FlyoutPage());
+
+        }
+
+        [RelayCommand]
+        public void ToggleTorch()
+        {
+            barcodeReader.IsTorchOn = !barcodeReader.IsTorchOn;
+        }
+
+        [RelayCommand]
+        public void SwapCamera()
+        {
+            if(barcodeReader.CameraLocation  == ZXing.Net.Maui.CameraLocation.Rear)
+            {
+                barcodeReader.CameraLocation = ZXing.Net.Maui.CameraLocation.Front;
+            }
+            else
+            {
+                barcodeReader.CameraLocation = ZXing.Net.Maui.CameraLocation.Rear;
+            }
         }
     }
 
+
 }
+
+
+    
