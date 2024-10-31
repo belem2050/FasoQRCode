211af56a7435b5babb6eed85fdbff1251b3f24cc
Moumouni BELEM <90107382+BELEMMoumouni@users.noreply.github.com>
2024-11-01 00:12:10 +0100

    feat: add History Item model

---
+++ FasoQRCode/Models/HistoryItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace FasoQRCode.Models
+{
+    public class HistoryItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.MinValue;
+        public string Content { get; set; } = String.Empty;
+        public string QrThumbnail { get; set; } = string.Empty;
+        
+
+        public HistoryItem(string title, DateTime date, string content, string qrThumbnail)
+        {
+            Title = title;
+            Date = date; 
+            Content = content;
+            QrThumbnail = qrThumbnail;
+        }
+
+        public HistoryItem()
+        {
+        }
+
+        
+
+
+    }
+
+
+
+}
