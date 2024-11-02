cf897b4e602523347c137c9dbb71271eedca4e82
Moumouni BELEM <90107382+BELEMMoumouni@users.noreply.github.com>
2024-11-03 00:36:14 +0100

    refactor: improve the way properties change events are handled

--- FasoQRCode/SystemManager.cs
+++ FasoQRCode/SystemManager.cs
@@ -3,6 +3,8 @@ using FasoQRCode.Models.Data;
 using FasoQRCode.ViewsModels;
 using System.Collections.ObjectModel;
 using ZXing;
+using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
 
 namespace FasoQRCode
 {
@@ -11,17 +13,43 @@ namespace FasoQRCode
         private static object _lockInstance = new object();
         static private SystemManager? _instance = null;
 
-        public SettingsVM Settings { get; set; } = new SettingsVM();
+        public PageSettingsVM Settings { get; set; } = new PageSettingsVM();
+
         public HistoryItem CurrentHistoryItem { get; set; } = new HistoryItem();
         public ObservableCollection<HistoryItem> HistoryItems { get; set; } = new ObservableCollection<HistoryItem>();
 
         private readonly HistoryService _historyService = new HistoryService();
 
+        private readonly IBarcodeReader barcodeReader;
+
 
         private SystemManager()
         {
+            _instance = this;
             HistoryItems = _historyService.LoadHistory();
             _historyService.SaveHistory(HistoryItems);
+
+            Settings.PropertyChanged += Settings_PropertyChanged;
+        }
+
+        private void Settings_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(PageSettingsVM.IsDarkModeEnabled))
+            {
+                Application.Current.UserAppTheme = Settings.IsDarkModeEnabled ? AppTheme.Dark : AppTheme.Light;
+            }
+
+            if (e.PropertyName == nameof(PageSettingsVM.Camera))
+            {
+                Settings.DefaultCamera = (Settings.Camera == "Rear") ? CameraLocation.Rear : CameraLocation.Front;
+            }
+
+            if (e.PropertyName == nameof(PageSettingsVM.IsTorchOn))
+            {
+              Settings.FlashIcon = Settings.IsTorchOn ? ImageSource.FromFile("flash.png") : ImageSource.FromFile("flash_off.png");
+            }
+
+            CameraView cameraView = new CameraView();
         }
 
         static public SystemManager GetInstance()
@@ -35,8 +63,5 @@ namespace FasoQRCode
                 return _instance;
             }
         }
-
-
-
     }
 }
